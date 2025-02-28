5be90c052ca3a41642f20e0007cf3844469bfa99
murali-N-tech <muralinaga826@gmail.com>
2025-02-28 20:31:13 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+document.querySelector('.navbar a[href="#contact"]').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' });
+  });
+
+  
+ 
+  
